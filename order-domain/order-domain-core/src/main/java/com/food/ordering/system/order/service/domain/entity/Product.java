ef4d80b24b17f790948642729409af0046060ee5
pokrepo <Myfirst1>
2023-05-21 19:08:15 +0200

    initial commit

---
+++ order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -0,0 +1,40 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+
+import java.util.Objects;
+
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(ProductId productId, String name, Money price) {
+        super.setId(productId);
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, price);
+    }
+}
